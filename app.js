ab4cf6faa2ea9c2f937ea917b217f0d0fa3ae276
Eugeeun <97161495+Eugeeun@users.noreply.github.com>
2022-04-02 21:33:24 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const abcd = null;
+let something;
+console.log(something, null);
+// what is difference between null and undefined?
+// null means empty place
+// undefined means literal "NOT DECLARED" = there's not value in it
+// but they mean false though
