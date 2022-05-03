4585fe92978307f34630ab1979017cb300d253f2
Eugeeun <97161495+Eugeeun@users.noreply.github.com>
2022-05-04 00:41:19 +0900

    JS using localStorage

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 'use strict';
 
-const abcd = null;
-let something;
-console.log(something, null);
-// what is difference between null and undefined?
-// null means empty place
-// undefined means literal "NOT DECLARED" = there's not value in it
-// but they mean false though
+const loginForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
+
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+if (!savedUsername) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // 브라우저의 기본 동작을 막음
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+  });
+} else {
+  paintGreetings(savedUsername);
+}
